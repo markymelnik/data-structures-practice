c6750c49ba55d2f1595533e8484e568687a82b25
markymelnik <markemelnik@gmail.com>
2023-01-26 17:02:09 -0500

    Stack Implementation

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ output.classList.add('output');
 container.append(output);
 
 output.textContent = "Output";
+
+// Stack Implementation
+
+class Stack {
+
+  constructor() { // Initializes an empty array.
+    this.items = [];
+  }
+
+  push(element) { // Adds element to the top of the stack.
+    this.items.push(element);
+  }
+
+  pop() { // Removes the top element from the stack.
+    if (this.items.length == 0) {
+      return "Underflow";
+    }
+    return this.items.pop();
+  }
+
+  peek() { // Returns the top element of the stack.
+    return this.items[this.items.length - 1];
+  }
+
+  size() { // Returns the size of the stack.
+    return this.items.length;
+  }
+
+  isEmpty() { // Returns true or false if the stack is non-empty or empty, respectively. 
+    return this.items.length == 0;
+  }
+
+  print() { // Returns a string in which all elements of the stack are concatenated.
+    let output = '';
+    for (let i = 0; i < this.items.length; i++) {
+      output += this.items[i] + '';
+    }
+    return output;
+    
+  }
+}
+
+const stack = new Stack();
