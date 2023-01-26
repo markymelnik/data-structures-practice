a4dd38bfbdabd249cf8102f3c5e7f8ca1fe32e5e
markymelnik <markemelnik@gmail.com>
2023-01-26 16:40:25 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Practice with Data Structures
+
+const container = document.querySelector('.container');
+const output = document.createElement('div');
+output.classList.add('output');
+container.append(output);
+
+output.textContent = "Output";
